ed21363dbab3b15f530f37d11aa013ea2e6bf611
jupiter9381 <jupiter9381@gmail.com>
2019-09-23 01:51:44 +1000

    tracks and majors imported

--- custom.js
+++ custom.js
@@ -3,12 +3,27 @@ $(document).ready(function(e){
 	var $categories = [];
 	var plans = [];
 
-	$.getJSON("/course_table_json/category.json", function(data) {
-		$category = data;
+ 	
+ 	$categories = ajaxCall("/course_table_json/category.json");
+	$plans = ajaxCall("/course_table_json/structure_plan.json");
+	$majors = ajaxCall("/course_table_json/structure_major.json");
+	$tracks = ajaxCall("/course_table_json/structure_track.json");
+
+	$plans.forEach(function($plan) {
+		console.log($plan);
 	});
 
-	$.getJSON("/course_table_json/structure_plan.json", function(data) {
-		$plans = data;
-		console.log($plans);
-	});
 });
+
+function ajaxCall(url) {
+	var data;
+	$.ajax({ 
+	    url: url, 
+	    dataType: 'json', 
+	    async: false, 
+	    success: function(json){ 
+	    	data = json;
+	    } 
+	});
+	return data;
+}
