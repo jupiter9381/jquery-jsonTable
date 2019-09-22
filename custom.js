a20a990953d72f2c9fd7056253be17c86a787d8c
jupiter9381 <jupiter9381@gmail.com>
2019-09-23 04:27:43 +1000

    draw Main Category

--- custom.js
+++ custom.js
@@ -4,31 +4,58 @@ $(document).ready(function(e){
 	var plans = [];
 
  	
+ 	$all_categories = ajaxCall("/course_table_json/category_desc.json");
  	$categories = ajaxCall("/course_table_json/category.json");
 	$plans = ajaxCall("/course_table_json/structure_plan.json");
 	$majors = ajaxCall("/course_table_json/structure_major.json");
 	$tracks = ajaxCall("/course_table_json/structure_track.json");
 
-	console.log($plans);
-	console.log($majors);
-	console.log($tracks);
-
+	$lang_id = 1;
 	$rows = [];
-	$row = [];
+	$plan_row = ["Plan"];
+	$major_row = ["Major"];
+	$track_row = ["Track"];
+	$tdNum = 0;
 	$plans.forEach(function($plan) {
 		$majors.forEach(function($major) {
 			if($major['plan_id'] == $plan['_id']){
 				$tracks.forEach(function($track){
 					if($track['major_id'] == $major['_id'] && $track['plan_id'] == $plan['_id']){
-						$row.push($plan['plan_name'][1]);
+						$tdNum++;
+						$plan_row.push($plan['plan_name'][$lang_id]);
+						$major_row.push($major['major_name'][$lang_id]);
+						$track_row.push($track['track_name']);
 					}
 				});
 			}
 		});
 	});
-	$rows.push($row);
+	$rows.push($plan_row);
+	$rows.push($major_row);
+	$rows.push($track_row);
 
 	drawTable($rows);
+
+	console.log($tdNum);
+
+
+
+	$categories.forEach(function($category){
+		$catRow = [];
+		if($category['parent_id'] == "0"){
+			$_id = $category['_id'];
+			$cat_detail = $all_categories.filter(cat => cat['cat_id'] == $_id && cat['lang_id'] == $lang_id)[0];
+			$catRow = [$cat_detail['name']];
+			drawCatTable($cat_detail['name'], $tdNum);
+			$parent_id = $_id;
+			while() {
+
+			}
+		}
+	});
+
+
+
 });
 
 function ajaxCall(url) {
@@ -45,7 +72,7 @@ function ajaxCall(url) {
 }
 
 function drawTable($rows) {
-	$html = "";
+	$html = $("table").html();
 	$rows.forEach(function($row){
 		$html += "<tr>";
 		for($i = 0; $i < $row.length; $i ++){
@@ -53,5 +80,16 @@ function drawTable($rows) {
 		}
 		$html += "</tr>";
 	});
+	$("table").html($html);
+}
+
+function drawCatTable($name, $num) {
+	$html = $("table").html();
+	$html += "<tr>";
+	$html += "<td>" + $name +"</td>";
+	for(var i = 0; i < $num; i++){
+		$html += "<td>" + "<input type='text' class='form-control'"+"</td>";
+	}
+	$html += "</tr>";
 	$("table").html($html);
 }
