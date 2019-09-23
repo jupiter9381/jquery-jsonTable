2a979f109348a2c6d2106a90b3aa3702509598d5
jupiter9381 <jupiter9381@gmail.com>
2019-09-24 00:30:02 +1000

    adding course row done

--- custom.js
+++ custom.js
@@ -3,14 +3,17 @@ $(document).ready(function(e){
 	var $categories = [];
 	var plans = [];
 
+	$lang_id = 1;
  	
  	$all_categories = ajaxCall("/course_table_json/category_desc.json");
  	$categories = ajaxCall("/course_table_json/category.json");
 	$plans = ajaxCall("/course_table_json/structure_plan.json");
 	$majors = ajaxCall("/course_table_json/structure_major.json");
 	$tracks = ajaxCall("/course_table_json/structure_track.json");
+	$courses = ajaxCall("/course_table_json/course.json");
 
-	$lang_id = 1;
+	drawCourseTable($courses);
+
 	$rows = [];
 	$plan_row = ["Plan"];
 	$major_row = ["Major"];
@@ -59,23 +62,16 @@ $(document).ready(function(e){
 			}
 		}
 	});
-
-	$('table tr.leaf').on('click', 'td', function (){
-		$status = $(this).children().attr('status');
-		if($status == "blank") {
-			$(this).children().attr({status: 'black'});
-			$(this).children().css({'background': 'black', 'border-color': 'black'});
-		} else if ($status == "black") {
-			$(this).children().attr({status: 'grey'});
-			$(this).children().css({'background': 'grey', 'border-color': 'black'});
-		} else if($status == "grey") {
-			$(this).children().attr({status: 'white'});
-			$(this).children().css({'background': 'white', 'border-color': 'black'});
-		} else  {
-			$(this).children().attr({status: 'blank'});
-			$(this).children().css({'background': 'white', 'border-color': 'white'});
-		}
+	$selectedRow = "";
+	$('#course_management tr td').on('click', 'button', function (){
+		$selectedRow = $(this).parent().parent();
 	});
+
+	$('#course_list tbody tr td').on('click', '.add_course', function (){
+		$index = $(this).attr('course-index');
+		drawCourse($index, $selectedRow);
+	});
+
 
 });
 
@@ -93,7 +89,7 @@ function ajaxCall(url) {
 }
 
 function drawTable($rows) {
-	$html = $("table").html();
+	$html = $("#course_management").html();
 	$rows.forEach(function($row){
 		$html += "<tr>";
 		for($i = 0; $i < $row.length; $i ++){
@@ -101,26 +97,67 @@ function drawTable($rows) {
 		}
 		$html += "</tr>";
 	});
-	$("table").html($html);
+	$("#course_management").html($html);
 }
 
 function drawCatTable($name) {
-	$html = $("table").html();
+	$html = $("#course_management").html();
 	$html += "<tr>";
 	$html += "<td>" + $name +"</td>";
 	for(var i = 0; i < $tdNum; i++){
 		$html += "<td>" + "<input type='text' class='form-control'"+"</td>";
 	}
 	$html += "</tr>";
-	$("table").html($html);
+	$("#course_management").html($html);
 }
 function drawLeafTable(){
-	$html = $("table").html();
-	$html += "<tr class='leaf'>";
+	$html = $("#course_management").html();
+	$html += "<tr>";
 	$html += "<td>" + "<button class='btn btn-primary' data-toggle='modal' data-target='#courseModal'>Add Course</button" +"</td>";
+	for(var i = 0; i < $tdNum; i++){
+		$html += "<td></td>";
+
+	}
+	$html += "</tr>";
+	$("#course_management").html($html);
+}
+function drawCourse($index, $obj){
+	$course = $courses[$index];
+	$html = "";
+	$html += "<tr class='leaf'>";
+	$html += "<td>" + $course['course_code'] + " "+ $course['name'][$lang_id] + "</td>";
 	for(var i = 0; i < $tdNum; i++){
 		$html += "<td><div class='cicle' status='blank' style='border: 1px solid #fff; padding: 10px 11px; background: white; border-radius: 50%; margin-left: auto; margin-right: auto; width: 1%;'></div></td>";
 	}
 	$html += "</tr>";
-	$("table").html($html);
+	$($html).insertBefore($obj);
+	$("#courseModal").modal('toggle');
+
+	$('#course_management tr.leaf').on('click', 'td', function (){
+		$status = $(this).children().attr('status');
+		if($status == "blank") {
+			$(this).children().attr({status: 'black'});
+			$(this).children().css({'background': 'black', 'border-color': 'black'});
+		} else if ($status == "black") {
+			$(this).children().attr({status: 'grey'});
+			$(this).children().css({'background': 'grey', 'border-color': 'black'});
+		} else if($status == "grey") {
+			$(this).children().attr({status: 'white'});
+			$(this).children().css({'background': 'white', 'border-color': 'black'});
+		} else if($status == "white") {
+			$(this).children().attr({status: 'blank'});
+			$(this).children().css({'background': 'white', 'border-color': 'white'});
+		}
+	});
+}
+function drawCourseTable($courses){
+	$html = "";
+	for($i = 0; $i < $courses.length; $i++){
+		$html += "<tr>";
+		$html += "<td><button type='button' class='btn btn-primary add_course' course-index='"+$i+"'>Add</button></td>";
+		$html += "<td>" + $courses[$i]['course_code'] + "</td>";
+		$html += "<td>" + $courses[$i]['name'][$lang_id] + "</td>";
+		$html += "</tr>";
+	}
+	$("#course_list tbody").html($html);
 }
